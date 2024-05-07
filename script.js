a5d7f7494371f31d9ab32858adcffd3d591bfe81
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-05-07 17:54:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-const inputs = document.querySelectorAll('.controls input');
+const player = document.querySelector('.player__video');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const playButton = document.querySelector('.player__button');
+const volumeSlider = document.querySelector('input[name="volume"]');
+const playbackRateSlider = document.querySelector('input[name="playbackRate"]');
+const skipButtons = document.querySelectorAll('[data-skip]');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function togglePlay() {
+  if (player.paused) {
+    player.play();
+    playButton.textContent = '❚ ❚';
+  } else {
+    player.pause();
+    playButton.textContent = '►';
+  }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateProgressBar() {
+  const percent = (player.currentTime / player.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function skip() {
+  player.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  player[this.name] = this.value;
+}
+
+function handleProgressClick(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * player.duration;
+  player.currentTime = scrubTime;
+}
+
+player.addEventListener('timeupdate', updateProgressBar);
+playButton.addEventListener('click', togglePlay);
+volumeSlider.addEventListener('input', handleRangeUpdate);
+playbackRateSlider.addEventListener('input', handleRangeUpdate);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+progress.addEventListener('click', handleProgressClick);
